8ad17a7e7c9c136c5bcd599d20b6c3ae77736113
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-08-29 12:06:29 +0000

    Added get_owner_username so name would show on posts and comments

--- chats/serializers.py
+++ chats/serializers.py
@@ -6,6 +6,9 @@ class ChatSerializer(serializers.ModelSerializer):
         model = Chat
         fields = ['id', 'owner', 'circle', 'content', 'timestamp']
 
+    def get_owner_username(self, obj):
+        return obj.owner.username
+
 class ChatDetailSerializer(ChatSerializer):
 
     circle = serializers.ReadOnlyField(source='circle.id')
