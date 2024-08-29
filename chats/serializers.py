882de5d9ea3db45220ac5f98cce115725e4d8f72
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-08-29 12:31:00 +0000

    Added owner_username to get username on comments

--- chats/serializers.py
+++ chats/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from .models import Chat
 
 class ChatSerializer(serializers.ModelSerializer):
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
+
+    
     class Meta:
         model = Chat
-        fields = ['id', 'owner', 'circle', 'content', 'timestamp']
+        fields = ['id', 'owner', 'circle', 'content', 'timestamp', 'owner_username']
 
     def get_owner_username(self, obj):
         return obj.owner.username
