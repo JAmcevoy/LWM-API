3752005c3c8d4a919436eb6e0fda34ec8c3dd5bc
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-08-30 14:57:12 +0000

    Cleaned up Cirlces urls

--- circles/urls.py
+++ circles/urls.py
@@ -3,6 +3,6 @@ from circles import views
 
 urlpatterns = [
     path('categories/', views.CategoryList.as_view()),
-    path('interestcircles/', views.InterestCircleList.as_view()),
-    path('interestcircles/<int:pk>/', views.InterestCircleDetail.as_view()),
+    path('interest-circles/', views.InterestCircleList.as_view()),
+    path('interest-circles/<int:pk>/', views.InterestCircleDetail.as_view()),
 ]
