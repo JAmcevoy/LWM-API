812fbfd37a1d6b922578aab928f04166862bae12
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-09-01 20:40:21 +0000

    Matched Circles view to other views - fix wrong imports

--- circles/views.py
+++ circles/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics, permissions
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Category, InterestCircle
 from .serializers import CategorySerializer, InterestCircleSerializer
 from learnwithme.permissions import IsOwnerOrReadOnly
