6b83ccca061f32b8ce1717470884d2257850ae39
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-09-01 18:32:08 +0000

    Configured changes for followers views and urls to work better with frontend

--- followers/urls.py
+++ followers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from followers import views
+from followers.views import FollowProfile, UnfollowProfile
 
 urlpatterns = [
-    path('followers/', views.FollowerList.as_view()),
-    path('followers/<int:pk>/', views.FollowerDetail.as_view())
+    path('profiles/<int:pk>/follow/', FollowProfile.as_view(), name='follow-profile'),
+    path('profiles/<int:pk>/unfollow/', UnfollowProfile.as_view(), name='unfollow-profile'),
 ]
