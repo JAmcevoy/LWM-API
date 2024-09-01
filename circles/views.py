834d218d914efe84f32d6248109ad613f37a7ce0
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-09-01 20:45:50 +0000

    Mistake chnaging the view to match others, issue corrected

--- circles/views.py
+++ circles/views.py
@@ -1,24 +1,14 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
+from learnwithme.permissions import IsOwnerOrReadOnly
 from .models import Category, InterestCircle
 from .serializers import CategorySerializer, InterestCircleSerializer
-from learnwithme.permissions import IsOwnerOrReadOnly
 
 
-class CategoryList(APIView):
+class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    def get(self, request):
-        category = Category.objects.all()
-        serializer = CategorySerializer(
-            category, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-class InterestCircleList(APIView):
+class InterestCircleList(generics.ListCreateAPIView):
     serializer_class = InterestCircleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
@@ -28,7 +18,7 @@ class InterestCircleList(APIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class InterestCircleDetail(APIView):
+class InterestCircleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = InterestCircle.objects.all()
     serializer_class = InterestCircleSerializer
     permission_classes = [IsOwnerOrReadOnly]
