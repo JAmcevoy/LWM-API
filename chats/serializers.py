7e0a94c65785f1ae69967a4ff5095abfe49d8fa6
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-09-10 12:14:42 +0000

    Added circle name for Chat title

--- chats/serializers.py
+++ chats/serializers.py
@@ -3,11 +3,12 @@ from .models import Chat
 
 class ChatSerializer(serializers.ModelSerializer):
     owner_username = serializers.CharField(source='owner.username', read_only=True)
+    chat_circle_name = serializers.ReadOnlyField(source='owner.profile.id')
 
     
     class Meta:
         model = Chat
-        fields = ['id', 'owner', 'circle', 'content', 'timestamp', 'owner_username']
+        fields = ['id', 'owner', 'circle', 'content', 'timestamp', 'owner_username', 'chat_circle_name']
 
     def get_owner_username(self, obj):
         return obj.owner.username
