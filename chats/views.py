45f4a70c7587dad6ede308bc461ce65d9f875cb1
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-09-11 17:41:07 +0000

    Mistake with the chatlist view

--- chats/views.py
+++ chats/views.py
@@ -19,7 +19,7 @@ class ChatList(generics.ListCreateAPIView):
         """
         circle_id = self.kwargs.get('circle_id')
         if circle_id:
-            return Chat.objects.filter(circle=circle_id).order_by('created_at')
+            return Chat.objects.filter(circle=circle_id)
         return Chat.objects.all().order_by('created_at')
 
     def perform_create(self, serializer):
